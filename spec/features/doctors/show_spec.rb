e3559aaeafde15cfa37f5571b8cdcd7bbec002fc
lance butler <lancej2004@gmail.com>
2024-03-05 10:08:32 -0700

    Feat: list doctors hospital and patients

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe "Doctor show" do
 
     @doctor_1 = @hospital.doctors.create!(name: "Roxy", specialty: "MRI", university: "MSU")
     @doctor_2 = @hospital.doctors.create!(name: "Jenna", specialty: "Liver", university: "CU Boulder")
+
+    @patient_1 = @doctor_1.patients.create!(name: "Lance", age: 19)
+    @patient_2 = @doctor_1.patients.create!(name: "Shiba", age: 7)
+    @patient_3 = @doctor_2.patients.create!(name: "Gojo", age: 23)
   end
 
   it "list a doctor and their attributes" do
@@ -18,5 +22,15 @@ RSpec.describe "Doctor show" do
     expect(page).to_not have_content("Jenna")
     expect(page).to_not have_content("Liver")
     expect(page).to_not have_content("CU Boulder")
+  end
+
+  it "displays doctors hospital and patients" do
+    visit doctor_path(@doctor_1)
+
+    expect(page).to have_content("UC Health")
+
+    expect(page).to have_content("Lance")
+    expect(page).to have_content("Shiba")
+    expect(page).to_not have_content("Gojo")
   end
 end
